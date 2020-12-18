2e77f0fc1da23cdc40234f550dede680f6a7f640
Sean Tarzy <seantarzy@gmail.com>
2020-12-18 03:51:41 -0500

    can create documents

--- index.js
+++ index.js
@@ -4,7 +4,34 @@ mongoose.connect("mongodb://localhost/playground", {
     useNewUrlParser: true,
     useUnifiedTopology: true,})
 .then(()=>console.log("connected"))
-.catch((err)=>console.log("could not connect to mongodb"))
+.catch((err)=>console.log("could not connect to mongodb", err))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+})
+
+const Course = mongoose.model("Course", courseSchema);
+
+
+async function createCourse(){
+
+    const course = new Course({
+        name: "angular.js COurse", 
+        author: "Sean", 
+        tags: ['angular', 'frontend'], 
+        isPublished: true
+    })
+    
+    const result = await course.save(); 
+    console.log(result)
+}
+
+createCourse()
+
 // mongoose
 //   .connect("mongodb://localhost/127.0.0.1:27017", {
 //     useNewUrlParser: true,
