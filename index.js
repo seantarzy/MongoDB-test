f38f68891ae6424245c3b9d8b431a5f436a307de
Sean Tarzy <seantarzy@gmail.com>
2020-12-18 03:28:18 -0500

    connected to mongo

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/playground")
-.then(()=>console.log("connected to MongoDB..."))
+mongoose.connect("mongodb://localhost/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,})
+.then(()=>console.log("connected"))
+.catch((err)=>console.log("could not connect to mongodb"))
+// mongoose
+//   .connect("mongodb://localhost/127.0.0.1:27017", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("connected to MongoDB..."))
+//   .catch((err) => console.log("could not connect to mongo db", err));
+
+
 
