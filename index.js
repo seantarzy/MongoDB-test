d9cbe82f2248d3d1f39b10e94e4061bbf6ee4dc9
Sean Tarzy <seantarzy@gmail.com>
2020-12-18 04:05:56 -0500

    comparison operators

--- index.js
+++ index.js
@@ -30,8 +30,28 @@ async function createCourse(){
     console.log(result)
 }
 
-createCourse()
 
+async function getCourses(){
+  const courses = await Course
+//   .find({price: {$gte: 10, $lse: 20}})
+.find({price: {$in: [10, 15, 20]}})
+  .limit(10)
+  .sort({name: 1})
+  .select({name: 1, tags: 1});
+   console.log(courses)
+}
+// createCourse()
+
+getCourses()
+//eq (equal)
+//ne (not equal)
+//gt (greater than)
+// lt (less than)
+// gt (greater than)
+// gte (greater than or equal to)
+// lte (less than or equal to)
+// in
+//nin (not in)
 // mongoose
 //   .connect("mongodb://localhost/127.0.0.1:27017", {
 //     useNewUrlParser: true,
